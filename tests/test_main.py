28ea5ee207d2145da92b35c11a30e22c8417ac6f
Dan Mihaila <danmihaila@gmail.com>
2024-05-09 15:41:07 +0300

    HDX-9801 - adapt smoketests with app_identifier

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,11 +2,11 @@ from io import StringIO
 import requests
 import pytest
 import csv
-from util.config import BASE_URL, TESTS_SPREADSHEET_URL, HEADER_API_CALL, HEADER_RULES, HEADER_DESCRIPTION, HEADER_ENABLED
+from util.config import BASE_URL, TESTS_SPREADSHEET_URL, HEADER_API_CALL, HEADER_RULES, HEADER_DESCRIPTION, \
+    HEADER_ENABLED, HAPI_APP_IDENTIFIER
 
 from util.requests import download_csv, read_data_from_csv
 from util.rules import parse_rules
-
 
 download_csv(TESTS_SPREADSHEET_URL, "tests.csv")
 data_all_columns = read_data_from_csv("tests.csv")
@@ -15,25 +15,32 @@ data = [[row[HEADER_DESCRIPTION], row] for row in data_all_columns if row[HEADER
 
 @pytest.mark.parametrize("description, test_info", data)
 def test_json_rest_api(description, test_info):
-    
     rules = parse_rules(test_info[HEADER_RULES])
 
-    relative_url = test_info[HEADER_API_CALL][1:] if test_info[HEADER_API_CALL].startswith('/') else test_info[HEADER_API_CALL]
+    relative_url = test_info[HEADER_API_CALL][1:] if test_info[HEADER_API_CALL].startswith('/') else test_info[
+        HEADER_API_CALL]
+    if '?' in relative_url:
+        relative_url += f'&app_identifier={HAPI_APP_IDENTIFIER}'
+    else:
+        relative_url += f'?app_identifier={HAPI_APP_IDENTIFIER}'
     endpoint_url = f'{BASE_URL}{relative_url}'
+
     response = requests.get(endpoint_url)
     object_list = response.json()
 
-    assert response.status_code == 200
+    assert response.status_code == 200, ", url:" + endpoint_url
 
     for rule in rules:
-        assert rule.operator(rule.input_list_builder(object_list), rule.value), rule.description
+        output_description = rule.description + ", url:" + endpoint_url
+        assert rule.operator(rule.input_list_builder(object_list), rule.value), output_description
+
 
 def test_csv_rest_api():
-    endpoint_url = f'{BASE_URL}api/admin1?output_format=csv'
+    endpoint_url = f'{BASE_URL}api/admin1?output_format=csv&app_identifier={HAPI_APP_IDENTIFIER}'
     response = requests.get(endpoint_url)
 
     assert response.status_code == 200
-    
+
     csv_data = response.text
 
     # Parse the CSV data into a list of rows
