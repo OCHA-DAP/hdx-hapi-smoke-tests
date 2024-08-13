bd057b5bcc67ca33d4eec537fe76854c6b0c9925
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-08-13 09:26:57 +0100

    HDX-10068 Removing existing legacy endpoint duplication tests

--- tests/test_duplicates.py
+++ tests/test_duplicates.py
@@ -7,6 +7,31 @@ from util.config import BASE_URL, HAPI_APP_IDENTIFIER
 
 from util.requests import fetch_data_from_hapi
 
+# ENDPOINT, Full count (2024-08-13), Country filter, Filtered count
+ENDPOINT_ROUTER_LIST = [
+    ("/api/v1/affected-people/refugees", 580074, "HND", 9140),
+    ("/api/v1/affected-people/humanitarian-needs", 279811, "HND", 2589),
+    ("/api/v1/coordination-context/operational-presence", 40472, "", None),
+    ("/api/v1/coordination-context/funding", 434, "", None),
+    ("/api/v1/coordination-context/conflict-event", 1544173, "HTI", 10081),
+    ("/api/v1/coordination-context/national-risk", 26, "", None),
+    ("/api/v1/food/food-security", 119757, "", None),
+    ("/api/v1/food/food-price", 1094401, "HTI", 15948),
+    ("/api/v1/population-social/population", 237100, "", None),
+    ("/api/v1/population-social/poverty-rate", 630, "", None),
+    ("/api/v1/metadata/dataset", 167, "", None),
+    ("/api/v1/metadata/resource", 257, "", None),
+    ("/api/v1/metadata/location", 250, "", None),
+    ("/api/v1/metadata/admin1", 455, "", None),
+    ("/api/v1/metadata/admin2", 5458, "", None),
+    ("/api/v1/metadata/currency", 128, "", None),
+    ("/api/v1/metadata/org", 2531, "", None),
+    ("/api/v1/metadata/org-type", 19, "", None),
+    ("/api/v1/metadata/sector", 20, "", None),
+    ("/api/v1/metadata/wfp-commodity", 1101, "", None),
+    ("/api/v1/metadata/wfp-market", 4141, "", None),
+]
+
 
 def test_fetch_data_from_hapi_with_paging():
     theme = "metadata/admin2"
@@ -100,33 +125,7 @@ def test_duplicates_humanitarian_needs_hnd_new_endpoint():
     assert len(results) == len(list(results_set))
 
 
-# ENDPOINT, Full count (2024-08-13), Country filter, Filtered count
-ENDPOINT_ROUTER_LIST = [
-    ("/api/v1/affected-people/refugees", 580074, "HND", 9140),
-    ("/api/v1/affected-people/humanitarian-needs", 279811, "HND", 2589),
-    ("/api/v1/coordination-context/operational-presence", 40472, "", None),
-    ("/api/v1/coordination-context/funding", 431, "", None),
-    ("/api/v1/coordination-context/conflict-event", 1544173, "HTI", 10081),
-    ("/api/v1/coordination-context/national-risk", 26, "", None),
-    ("/api/v1/food/food-security", 119876, "", None),
-    ("/api/v1/food/food-price", 1094401, "HTI", 15948),
-    ("/api/v1/population-social/population", 237337, "", None),
-    ("/api/v1/population-social/poverty-rate", 630, "", None),
-    ("/api/v1/metadata/dataset", 167, "", None),
-    ("/api/v1/metadata/resource", 257, "", None),
-    ("/api/v1/metadata/location", 250, "", None),
-    ("/api/v1/metadata/admin1", 455, "", None),
-    ("/api/v1/metadata/admin2", 5458, "", None),
-    ("/api/v1/metadata/currency", 128, "", None),
-    ("/api/v1/metadata/org", 2531, "", None),
-    ("/api/v1/metadata/org-type", 19, "", None),
-    ("/api/v1/metadata/sector", 20, "", None),
-    ("/api/v1/metadata/wfp-commodity", 1101, "", None),
-    ("/api/v1/metadata/wfp-market", 4144, "", None),
-]
-
-
-def test_endpoint_list():
+def test_endpoint_list_against_openapi_definition():
     with request.urlopen(f"{BASE_URL}/openapi.json") as openapi_json_url:
         openapi_json = json.load(openapi_json_url)
 
@@ -139,6 +138,27 @@ def test_endpoint_list():
     assert endpoint_paths == openapi_paths
 
 
-@pytest.mark.parametrize("endpoint_router", ENDPOINT_ROUTER_LIST)
-def test_for_duplicates(endpoint_router):
-    pass
+@pytest.mark.parametrize(
+    "endpoint_router",
+    ENDPOINT_ROUTER_LIST,
+    ids=[x[0][7:] for x in ENDPOINT_ROUTER_LIST],
+)
+def test_for_duplicates_all_endpoints_parametrically(endpoint_router):
+    theme = endpoint_router[0][1:]
+    country = endpoint_router[2]
+
+    query_url = (
+        f"{BASE_URL}{theme}?"
+        f"output_format=csv"
+        f"&location_code={country}"
+        f"&app_identifier={HAPI_APP_IDENTIFIER}"
+    )
+
+    if "refugees" in query_url:
+        query_url = query_url.replace("location_code", "origin_location_code")
+
+    results = fetch_data_from_hapi(query_url, limit=1000)
+
+    results_set = set(results)
+
+    assert len(results) == len(list(results_set))
