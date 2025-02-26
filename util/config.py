a4966bb895ded2335037ac7263f78d11c096a157
Dan Mihaila <danmihaila@gmail.com>
2025-02-26 11:21:36 +0200

    Update config.py

--- util/config.py
+++ util/config.py
@@ -5,7 +5,7 @@ BASE_URL = os.getenv('BASE_URL', 'https://HAPI_SERVER_URL/')
 HAPI_APP_IDENTIFIER = os.getenv('HAPI_APP_IDENTIFIER', '')
 TESTS_SPREADSHEET_URL = os.getenv(
     'TESTS_SPREADSHEET_URL',
-    'https://docs.google.com/spreadsheets/d/e/2PACX-1vSfC6zhjjg1MmwoBV2swtKt3mIseFLkzwHgsdJkt6E7wyGW0JdgoV_sbQhlk1CFp8BIt4cJ5-_lKKn9/pub?gid=1172486952&single=true&output=csv',
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vSfC6zhjjg1MmwoBV2swtKt3mIseFLkzwHgsdJkt6E7wyGW0JdgoV_sbQhlk1CFp8BIt4cJ5-_lKKn9/pub?gid=185760317&single=true&output=csv',
 )
 
 HEADER_API_CALL = 'API call'
